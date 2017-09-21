908dbaedc36d40da84e4bd60d89cae8dada8b445
Sneha68 <32151282+Sneha68@users.noreply.github.com>
2017-09-20 22:37:44 -0400

    adding comments

--- src/main/java/com/monsanto/mbt/ShipmentUtil.java
+++ src/main/java/com/monsanto/mbt/ShipmentUtil.java
@@ -5,30 +5,34 @@ import java.util.Comparator;
 import java.util.List;
 
 public class ShipmentUtil {
-	
+
 	private static WidgetDateComparator wdc = new WidgetDateComparator();
 	private static WidgetColorComparator wcc = new WidgetColorComparator();
-	
+
+  //logic to create shipment
 	private static Shipment createShipment(List<Widget> widgets, Comparator<Widget> widgetCompare) {
-		Shipment shipment = null;	
+		Shipment shipment = null;
+		//if widgets are more than 10 then return null and log message
 		if(widgets.size()>10){
 			System.out.println("Only 10 widgets can be added to shipment");
 		}else{
 			Collections.sort(widgets, widgetCompare);
 			shipment = new Shipment();
 			shipment.setWidgets(widgets);
-		}		
+		}
 		return shipment;
 	}
-	
-	public static Shipment createShipmentSortDate(List<Widget> widgets) {		
+
+	//method to create shipment with widgets sorted by date
+	public static Shipment createShipmentSortDate(List<Widget> widgets) {
 		Shipment shipment = createShipment(widgets, wdc);
 		return shipment;
 	}
-	
+
+	//method to create shipment with widgets sorted by color
 	public static Shipment createShipmentSortColor(List<Widget> widgets) {
 		Shipment shipment = createShipment(widgets, wcc);
 		return shipment;
-	}	
+	}
 
 }
