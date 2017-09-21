2ff67537f1f36b9c77c46f606b165ed8e937126a
Sneha68 <32151282+Sneha68@users.noreply.github.com>
2017-09-20 22:29:11 -0400

    Adding test cases for coding test

--- src/test/java/com/monsanto/mbt/ShipmentTest.java
+++ src/test/java/com/monsanto/mbt/ShipmentTest.java
@@ -1,5 +1,6 @@
 package com.monsanto.mbt;
 
+import java.util.List;
 
 import org.junit.Test;
 
@@ -7,18 +8,34 @@ import static org.junit.Assert.assertTrue;
 
 public class ShipmentTest {
 
-    @Test
-    public void testShipment_Sorted_By_Color(){
-        //Implement your solution to get list of shipments
+	@Test
+	public void testShipment_Sorted_By_Color() {
+		// Implement your solution to get list of shipments
+		List<Widget> testWidgets = WidgetUtils.getSampleWidgets().subList(0, 10);
+		Shipment shipmentColor = ShipmentUtil
+				.createShipmentSortColor(testWidgets);
+		List<Widget> shipmentWidgets = shipmentColor.getWidgets();		
+		assertTrue(shipmentWidgets.get(0).getColor().equals("Blue")
+				&& shipmentWidgets.get(1).getColor().equals("Blue")
+				&& shipmentWidgets.get(4).getColor().equals("Green")
+				&& shipmentWidgets.get(7).getColor().equals("Red"));
+	}
 
-    }
-
-    @Test
-    public void testShipment_Sorted_By_Date(){
-        //Implement your solution to get list of shipments
-
-
-    }
-
+	@Test
+	public void testShipment_Sorted_By_Date() {
+		// Implement your solution to get list of shipments
+		List<Widget> testWidgets = WidgetUtils.getSampleWidgets().subList(0, 10);		
+		Shipment shipmentDate = ShipmentUtil
+				.createShipmentSortDate(testWidgets);
+		List<Widget> shipmentWidgets = shipmentDate.getWidgets();
+		assertTrue(shipmentWidgets.get(0).getProductionDate()
+				.equals(WidgetUtils.stringToDateConverter("09-01-2005")) &&
+				shipmentWidgets.get(1).getProductionDate()
+				.equals(WidgetUtils.stringToDateConverter("01-01-2006")) &&
+				shipmentWidgets.get(2).getProductionDate()
+				.equals(WidgetUtils.stringToDateConverter("03-09-2006"))&&
+				shipmentWidgets.get(9).getProductionDate()
+				.equals(WidgetUtils.stringToDateConverter("06-02-2016")));				
+	}
 
 }
