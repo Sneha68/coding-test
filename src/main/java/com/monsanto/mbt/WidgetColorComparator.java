55bc87b7dfc414291f0fd1e9c662ed00f59ef179
Sneha68 <32151282+Sneha68@users.noreply.github.com>
2017-09-20 22:31:22 -0400

    comments added

--- src/main/java/com/monsanto/mbt/WidgetColorComparator.java
+++ src/main/java/com/monsanto/mbt/WidgetColorComparator.java
@@ -6,6 +6,7 @@ public class WidgetColorComparator implements Comparator<Widget> {
 
 	@Override
 	public int compare(Widget w1, Widget w2) {
+	//logic to compare widgets based on color, null scenarios handled	
 		int compareValue = 0;
 		if(w1==null && w2!=null){
 			compareValue= -1;
